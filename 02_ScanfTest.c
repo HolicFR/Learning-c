b1219fec4210db027ee597499609b66af83d8159
HolicFR <manas260364@gmail.com>
2026-07-23 22:15:40 +0530

    changed the value of variable

---
+++ 02_ScanfTest.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main() {
+    int a;
+    printf("Enter Your Age : ");
+    scanf("%d", &a);
+
+    printf("Your Age is : %d\n", a);
+
+
+    return 0;
+}
---
+++ 02_variablesadd.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+ int main() {
+    int a = 10;
+    int b = 20;
+
+    printf("The sum of a and b is: %d\n", a+b);
+    printf("The difference of a and b is: %d\n", a-b);
+    return 0;
+
+ }
