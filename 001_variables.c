109aecc30cf3dac2545df7a80015c902e6fc3a1c
HolicFR <manas260364@gmail.com>
2026-07-23 22:13:17 +0530

    Add 001_variables.c

---
+++ 001_variables.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+ int main () {
+   int a;
+   int b;
+   a=1000;
+   b=2000;
+   printf("The sum of a and b is %d\n", a+b);
+    return 0;
+ }
+
+ /*this is a Test code i ran. This shows what a variable is and how to use it. to sum up an integer value we  use variables and provide the integers a numeric value that i will refer to as
+ the values of integer.*/
