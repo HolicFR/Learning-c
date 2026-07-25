c66845323243e5e2a84203f9d316f8e3ee3bf51c
Manas Tanmay <manas260364@gmail.com>
2026-07-25 23:33:09 +0530

    Format_Specifiers

---
+++ 03_Data_Types/Format_Specifiers.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main() {
+ 
+    int myNum = 5;
+    float myfloatNum = 5.90;
+    char myChar = 'M';
+
+    printf("My Number is %d\n", myNum);
+    printf("My float Number is %f\n", myfloatNum);
+    printf("My name starts with %c\n", myChar);
+
+    return 0;
+}
