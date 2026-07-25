513dad95f3a83515a6b8ab528da465518ec48d72
Manas Tanmay <manas260364@gmail.com>
2026-07-26 00:03:24 +0530

    VariablesSum

---
+++ 03_Data_Types/VariablesSum.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+ int main(  ) {
+    int x = 5125;
+    int y = 1512412;
+    int sum = x + y;
+
+     printf("The sum of %d and %d is : %d", x, y, sum);
+     
+
+     return 0;
+
+
+ }
