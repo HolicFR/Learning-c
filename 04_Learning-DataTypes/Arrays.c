fd465ff63e475edd57854059bc55d4772329c17c
Manas Tanmay <manas260364@gmail.com>
2026-07-28 23:41:26 +0530

    Tried Arrays

---
+++ 04_Learning-DataTypes/Arrays.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+ 
+int main() {
+
+    int marks[5] = {10,20,30,40,50};
+
+    printf("%d\n", marks[0]);
+    printf("%d\n", marks[1]);
+    printf("%d\n", marks[2]);
+    printf("%d\n", marks[3]);
+    printf("%d\n", marks[4]);
+
+    return 0;
+
+}
