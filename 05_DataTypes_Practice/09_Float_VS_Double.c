5329b1624df9d940f22391e21659087f4d5d3ba0
Manas Tanmay <manas260364@gmail.com>
2026-07-30 00:16:28 +0530

    Scientific Numbers note + EX

--- 05_DataTypes_Practice/09_Float_VS_Double.c
+++ 05_DataTypes_Practice/09_Float_VS_Double.c
@@ -10,3 +10,13 @@
     return 0;
 
 }
+
+/*In C, you can write very large or very small floating-point numbers using scientific notation.
+
+This is done using the letter e (or E), which stands for "times 10 to the power of".
+
+For example, 35e3 means 35 × 10³ = 35000.
+
+This is useful for writing numbers in a shorter way. 
+Especially when working with scientific values or large-scale data.
+*/
