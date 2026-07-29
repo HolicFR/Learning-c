365b86aced6449f713d351514afde6addef875c4
Manas Tanmay <manas260364@gmail.com>
2026-07-29 23:42:05 +0530

    Multiple Char Characters VIA strings

---
+++ 05_DataTypes_Practice/05_MultiCharCharacters.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+ int main() {
+
+    char myText[] = "Hello";
+    printf("%s"), myText);
+
+    return 0;
+}
+
+//To store multiple characters (or whole words), use strings
+//Will learn more about *STRINGS* later
