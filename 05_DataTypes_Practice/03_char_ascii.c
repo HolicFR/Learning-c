f52efd9eff83992554d66d646367bd289156246a
Manas Tanmay <manas260364@gmail.com>
2026-07-29 22:59:09 +0530

    Char_Ascii and ASCII Printable Characters

---
+++ 05_DataTypes_Practice/03_char_ascii.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+ int main() {
+
+    char A = 65, B = 66, C = 67; 
+    printf("Ashwin has scored \"%c\" Marks in science", A);
+    printf(" \"%c\" Marks in maths", B);
+    printf(" \"%c\" Marks in telugu", C);
+    
+    return 0;
+//Here char stores numbers as characters. here is a list of all the numbers 
+//https://www.w3schools.com/charsets/ref_html_ascii.asp
+}
