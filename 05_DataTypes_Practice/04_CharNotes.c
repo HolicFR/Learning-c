8f4678014359d968347834bbdecc6a0fdbe49931
Manas Tanmay <manas260364@gmail.com>
2026-07-29 23:38:44 +0530

    Char Rule

---
+++ 05_DataTypes_Practice/04_CharNotes.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+ int main() {
+
+    char myText = 'Hello';
+        printf("%c", myText);
+
+    
+    return 0;
+//If you try to store more than a single character, it will only print the last character
+
+//Output here was "O".
+
+}
