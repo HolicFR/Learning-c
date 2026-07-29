508a2e0934fb4e77b2388915df2042b89a0b29ae
Manas Tanmay <manas260364@gmail.com>
2026-07-29 22:42:14 +0530

    Char and Format Specifier Revised

---
+++ 05_DataTypes_Practice/02_Char.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+ int main () {
+
+    char myGrade = 'A' ;
+
+    printf("He's got an \"%c\" for his performance.", myGrade); //To print with a format specifier use ("") between the sentence in printf before seperating the line with the variable like (myGrade)
+    
+
+    //If you want the format specifier output to be in ("") commas use \"(FORMAT SPECIFIER HERE)\" this format.
+    
+        return 0;
+
+ }
