30342b08d81c5484308a92ff54a296466b4921cf
Manas Tanmay <manas260364@gmail.com>
2026-07-29 22:34:41 +0530

    Basic Data Types Practice

---
+++ 05_DataTypes_Practice/01_Practice.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+ int main() { 
+
+    int myNum = 5;
+    float myFloatNum = 5.88;
+    char myLetter = 'D';
+
+    printf("%d\n", myNum);
+    printf("%.2f\n", myFloatNum);
+    printf("%c\n", myLetter);
+
+        return 0;
+
+ }
+
+ // This was a practice for Basic DATATYPES.
+ 
