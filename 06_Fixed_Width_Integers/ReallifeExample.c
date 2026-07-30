d73e51434ba5ed3dbe56262d79e6c1099896a0b3
Manas Tanmay <manas260364@gmail.com>
2026-07-30 20:22:23 +0530

    Battery Percentage With uint

---
+++ 06_Fixed_Width_Integers/ReallifeExample.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <stdint.h>
+
+ int main() {
+    uint8_t battery = 89; // Battery level percentage
+
+    printf("Battery level is %u out of 100\n", battery);
+    return 0;
+    
+ }
