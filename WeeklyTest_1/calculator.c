6849ae41d1eb5a668132d18cb7b52265472a2351
Manas Tanmay <manas260364@gmail.com>
2026-07-31 00:55:27 +0530

    Simple Calculator Test

---
+++ WeeklyTest_1/calculator.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+ int main() {
+    int a = 5, b = 3;
+
+    printf("Sum: %d\n", a + b);
+    printf("Difference: %d\n", a - b);
+    printf("Product = %d\n", a*b);
+    return 0;
+
+ }
+
+ /*Question was : 1. Calculator
+
+Input two integers and print:
+
+Sum
+Difference
+Product
+
+Example
+
+Input:
+5 3
+
+Output:
+Sum = 8
+Difference = 2
+Product = 15*/
