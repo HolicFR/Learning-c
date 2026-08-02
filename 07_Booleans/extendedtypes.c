092c9c8dfc4abbff5130ce100bbceb93e72c595f
Manas Tanmay <manas260364@gmail.com>
2026-08-02 22:50:36 +0530

    Check Size

--- 07_Booleans/extendedtypes.c
+++ 07_Booleans/extendedtypes.c
@@ -19,5 +19,14 @@ printf("Very Big: %lld\n", veryBig);
 printf("Huge: %llu\n", huge);
 printf("Precise: %Lf\n", precise);
 
+printf("Size of int: %zu bytes\n", sizeof(int));
+printf("Size of double: %zu bytes\n", sizeof(double));
+printf("Size of short int: %zu bytes\n", sizeof(short int));
+printf("Size of unsigned int: %zu bytes\n", sizeof(unsigned int));
+printf("Size of long int: %zu bytes\n", sizeof(long int));
+printf("Size of long long int: %zu bytes\n", sizeof(long long int));
+printf("Size of unsigned long long int: %zu bytes\n", sizeof(unsigned long long int));
+printf("Size of long double: %zu bytes\n", sizeof(long double));
+
 return 0;
 }
