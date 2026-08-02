8f9c7f5ab01a3fce2a95608710bfeb27d67e3e62
Manas Tanmay <manas260364@gmail.com>
2026-08-02 23:57:34 +0530

    Float sum

--- 07_Booleans/charwithcfloat.c
+++ 07_Booleans/charwithcfloat.c
@@ -4,10 +4,10 @@
 
         int i = 17;
         char c = 'c'; /* ASCII VALUE IS 99 */
-        int sum;
+        float sum;
 
         sum = i + c;
-        printf("Value of sum : %d\n", sum);
+        printf("Value of sum : %f\n", sum);
 
         return 0;
         
