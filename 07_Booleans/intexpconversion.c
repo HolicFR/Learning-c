9131d1ac1f532072ff116f0ce32452a222b741de
Manas Tanmay <manas260364@gmail.com>
2026-08-02 23:22:37 +0530

    INT EXPLICIT CONVERSION

---
+++ 07_Booleans/intexpconversion.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+    int main() {
+        int a = 10;
+        int b = 20;
+
+        float sum = (float) a/b;
+
+        printf("%.1f\n", sum);
+
+        return 0;
+        
+    }
