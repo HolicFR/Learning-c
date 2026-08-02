ed9ffa265dc19cf5d4802d9391852c1565af642d
Manas Tanmay <manas260364@gmail.com>
2026-08-02 22:10:13 +0530

    Boolean Data Type

---
+++ Booleans.c
@@ -0,0 +1,17 @@
+#include <stdio.h> 
+#include <stdbool.h>  // INCLUDE THIS TO USE BOOLEAN DATA TYPE
+
+int main() {
+
+    bool IsProgrammingFun = true;
+    bool WillIFindTrueLove = false;     
+
+    // Print boolean values
+    
+    printf("%d\n", IsProgrammingFun);
+    printf("%d", WillIFindTrueLove);    
+
+    return 0;
+
+}
+
