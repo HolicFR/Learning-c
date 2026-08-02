d64bd224d7f532a3c8448880ac0d652276f31d6b
Manas Tanmay <manas260364@gmail.com>
2026-08-02 22:22:39 +0530

    Password Authetication

---
+++ 07_Booleans/PasswordAutheticator.c
@@ -0,0 +1,22 @@
+#include <stdio.h> 
+#include <stdbool.h>
+
+int main(void) {
+    int correctPassword = 12345;
+    int enteredPassword;
+
+    printf("Enter Your Password: ");
+    scanf("%d", &enteredPassword);
+
+    bool passwordCorrect = (enteredPassword == correctPassword);
+
+    if (passwordCorrect) {
+        printf("Login Succesfull");
+
+    } else {
+        printf("Wrong Password!\n");
+
+    }    
+        return 0;
+
+}
