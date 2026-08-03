33b6d8cb286be71a4035ca318415141c307a8b95
Manas Tanmay <manas260364@gmail.com>
2026-08-03 15:45:17 +0530

    Volume of cylinder

---
+++ Practice Set 1/04_Practice.c
@@ -0,0 +1,13 @@
+/* calculate the volume of
+a cylinder given its radius and height */
+
+#include <stdio.h>
+
+int main() {
+    int r = 10, h = 20;
+        
+    printf("The volume of cylinder is %.1f", 3.14*r*r*h);
+
+    return 0;
+}
+
