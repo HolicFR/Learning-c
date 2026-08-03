f163adae70c55584895878a56d1e0b83f4cb3146
Manas Tanmay <manas260364@gmail.com>
2026-08-03 15:51:37 +0530

    Custom Input value to measure volume of cylinder

---
+++ Practice Set 1/05_Practice.c
@@ -0,0 +1,18 @@
+// Custom Values To measure
+
+#include <stdio.h>
+
+int main() {
+
+    int r;
+    int h;
+
+        printf("Input Radius of the circle: ");
+        scanf("%d", &r);
+
+        printf("Input Height: ");
+        scanf("%d", &h);
+
+            printf("The volume of cylinder is: %.1f\n", 3.14*r*r*h);
+    return 0;
+}
