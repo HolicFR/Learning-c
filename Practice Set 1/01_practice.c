af8a948c542548d134fe7b208e0a5b83b7223008
Manas Tanmay <manas260364@gmail.com>
2026-08-03 15:30:51 +0530

    Hard Coded Inputs

---
+++ Practice Set 1/01_practice.c
@@ -0,0 +1,14 @@
+/*Write a C program to calculate the area of a rectangle:
+a. Using hard coded inputs.
+b. Using inputs supplied by the user.*/
+
+#include <stdio.h>  
+    int main() {
+
+        int length = 10;
+        int breadth = 20;
+
+            printf("The area of Rectangle is: %d\n", length*breadth);
+
+            return 0;
+    }
