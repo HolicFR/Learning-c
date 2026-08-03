884ce3016a13cbdcae878901064dc90adb79de7f
Manas Tanmay <manas260364@gmail.com>
2026-08-03 23:44:23 +0530

    Student Info

---
+++ Practice Set 1/13_Practice.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main() {
+    int age = 19;
+    float percentage = 87.5;
+    char grade = 'A';
+
+    printf("Age: %d\n", age);
+    printf("Percentage: %.1f%%\n", percentage);
+    printf("Grade: %c\n", grade);
+
+    return 0;
+}
