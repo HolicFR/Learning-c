3a1bcc69844eb85959b815429ac6ef9781c5bb17
Manas Tanmay <manas260364@gmail.com>
2026-08-03 15:39:32 +0530

    Area of Circle

---
+++ Practice Set 1/03_Practice.c
@@ -0,0 +1,12 @@
+/*2. Calculate the area of a circle and modify the same program to calculate the volume of
+a cylinder given its radius and height.*/
+
+#include <stdio.h>
+
+int main() {
+
+    int r = 10;
+    printf("The area of the circle is %.1f", 3.14*r*r);
+    
+    return 0;
+}
