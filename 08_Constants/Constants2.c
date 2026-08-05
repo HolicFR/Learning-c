be3af8816ce383f108f8e34376bd39fa45eb45b4
Manas Tanmay <manas260364@gmail.com>
2026-08-05 23:32:58 +0530

    Constants Correct Usage

---
+++ 08_Constants/Constants2.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main() {
+const int minutesperhour = 60;
+const int monthsinyear = 12;
+
+    printf("There are %d minutes per hour \n", minutesperhour);
+    printf("There are %d Months in a Year", monthsinyear);
+    return 0;
+}
+
+// This is the right usage of *Constants* in C
