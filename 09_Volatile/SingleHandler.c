86baedb74468c07290daa641793931ff2092059d
Manas Tanmay <manas260364@gmail.com>
2026-08-05 23:58:43 +0530

    Signal Handler Example. SAFE TO RUN

--- 09_Volatile/SingleHandler.c
+++ 09_Volatile/SingleHandler.c
@@ -21,3 +21,29 @@ int main(void) {
 
     return 0;
 }
+
+// HERE 
+// volatile sig_atomic_t stop = 0;
+// The normal execution of main() doesn't change stop
+// 
+// 
+//  INSTEAD : 
+/*       main()
+ │
+ ▼
+stop = 0
+ │
+ ▼
+while (!stop)
+ │
+ │     User presses Ctrl+C
+ │             ↓
+ │       handleSignal()
+ │             ↓
+ │         stop = 1
+ │             ↓
+ └────────► loop ends*/ 
+// This is a much better demonstration of why volatile exists.
+
+sig_atomic_t is also used because it is a type intended for safe access in this signal-handler context.
+
