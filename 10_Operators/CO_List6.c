4d50986e8abf0ca5289d66e44611147ae632a80c
Manas Tanmay <manas260364@gmail.com>
2026-08-06 21:26:49 +0530

    Real Life Examples

--- 10_Operators/CO_List6.c
+++ 10_Operators/CO_List6.c
@@ -1,1 +1,11 @@
-#include
+#include <stdio.h>
+
+int main() {
+    int passwordLength = 5;
+
+        printf("%d\n", passwordLength >= 18); // 0 (False), Too short
+        
+        printf("%d\n", passwordLength < 8); // 1 (True), needs more characters
+
+    return 0;
+}   
