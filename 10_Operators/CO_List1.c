6438949a1ca7d285d62cce34fef40675cadc3705
Manas Tanmay <manas260364@gmail.com>
2026-08-06 21:08:19 +0530

    != Operator

---
+++ 10_Operators/CO_List1.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int main() {
+    int x = 10;
+    int y = 20;
+
+        printf("%d", x != y);       // *!x* returns 1 if the values are not equal. 
+    return 0;
+}
---
+++ 10_Operators/CO_List2.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main() {
+    int x = 10;
+    int y = 20; // USING > (Greater than)
+
+    printf("%d", x > y); // Returns 1 if the first value is greater than the second value
+                        
+
+                    // Here it returns 0 because the value of X is less than Y 
+    return 0;
+}
