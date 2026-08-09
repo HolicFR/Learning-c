1021d9df7ba03bd3b6ebbdbce87ea7f28bc5591e
Manas Tanmay <manas260364@gmail.com>
2026-08-09 23:22:52 +0530

    Odd or even

---
+++ 11_Ternary Operators Example/E_O.c
@@ -0,0 +1,14 @@
+// Even or Odd Example
+
+#include <stdio.h>  
+int main() {
+    int n;
+
+    printf("Enter a number: ");
+    scanf("%d", &n);
+
+    (n % 2 == 0) ? printf("Even") : printf("Odd");
+
+    return 0;
+    
+}
