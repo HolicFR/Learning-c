abb0b3356efe9b0e1881777b9943493325270d33
Manas Tanmay <manas260364@gmail.com>
2026-08-10 23:47:11 +0530

    Condition Positive Negative

---
+++ Ternary_If_Else/Conditions/Pos_Neg.c
@@ -0,0 +1,12 @@
+// 6. Check if number is positive or negative
+#include <stdio.h>
+
+int main() {
+    int num = -10;
+
+    // If num >= 0 → Positive
+    // Otherwise → Negative
+    (num >= 0) ? printf("Positive") : printf("Negative");
+
+    return 0;
+}
