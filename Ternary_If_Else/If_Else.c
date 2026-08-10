878feb03daf15da1bfdcfdc9b433c562edbc62ee
Manas Tanmay <manas260364@gmail.com>
2026-08-10 23:43:17 +0530

    If Else Ternary Revise

---
+++ Ternary_If_Else/If_Else.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main() {
+    int num = -5; 
+
+     // Check if num is greater than or equal to 0
+     if (num >= 0) {
+        printf("Positive");
+     } else {
+        printf("Negative");
+     }
+     
+    return 0;
+}
