bfb8d67973abc79b6ce08ec5411d052bdfa3592f
Manas Tanmay <manas260364@gmail.com>
2026-08-10 23:44:53 +0530

    If Else Adult/Minor

---
+++ Ternary_If_Else/IF_Adult.c
@@ -0,0 +1,14 @@
+// Check if someone is adult
+
+#include <stdio.h> 
+    int main() {
+            int age = 20;
+
+            // 18 or above = adult
+            if (age >= 18) {
+                printf("adult");
+            } else {
+                printf("Minor");
+            }
+            return 0;
+    }
