083da28c61c61794845dbe5ecbe0fd24bde7e8da
Manas Tanmay <manas260364@gmail.com>
2026-08-10 23:45:42 +0530

    If Else Biggest Number

---
+++ Ternary_If_Else/Biggest_Number.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main() {
+    int a = 15;
+    int b = 20;
+
+    // Compare a and b
+    if (a > b) {
+        printf("a is bigger");
+    } else {
+        printf("b is bigger");
+    }
+
+    return 0;
+}
