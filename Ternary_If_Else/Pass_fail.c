c6f067bd9ffc45241f7b03aab9ca40268a93f855
Manas Tanmay <manas260364@gmail.com>
2026-08-10 23:46:15 +0530

    If Else Student passed

---
+++ Ternary_If_Else/Pass_fail.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main() {
+    int marks = 65;
+
+    // 40 or above = pass
+    if (marks >= 40) {
+        printf("Pass");
+    } else {
+        printf("Fail");
+    }
+
+    return 0;
+}
