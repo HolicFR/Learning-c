7e454cf6f4d0adbefa82528b68725c92aac3ae18
Manas Tanmay <manas260364@gmail.com>
2026-08-11 20:24:18 +0530

    Self If/Else Test

---
+++ 12_If_Else_Statements.c/Example1.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main() {
+
+    int a;
+
+    printf("Enter your Marks Out of 50: ");
+    scanf("%d", &a);
+
+    if (a >= 50) {  printf("You are pass");
+    } else {
+        printf("Fail");
+
+    }
+
+
+
+
+    return 0;
+}
