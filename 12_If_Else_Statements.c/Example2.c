89235d84a4539ee0284822bd0cc0b0562eb4f13f
Manas Tanmay <manas260364@gmail.com>
2026-08-11 20:31:00 +0530

    If Statement with logical operators

--- 12_If_Else_Statements.c/Example2.c
+++ 12_If_Else_Statements.c/Example2.c
@@ -1,15 +1,21 @@
 #include <stdio.h>
+ 
+int main (){
 
-int main() {
-
-    int a;
-
-    a = 12;
-    printf("Value of a is: %d\n", a);
-
-    if(a < 20){
-        printf("a is less than 20\n");
-    }
-    
-    return 0;
+   /* local variable declaration */
+   int a;
+   
+   // run the program for different values of "a" 
+   // Assign 12 first and 40 afterwards
+   
+   a = 40;  //change to 40 and run again
+   printf("Value of a is : %d\n", a);
+ 
+   // check the boolean condition using if statement
+	
+   if(a < 20){
+      //if the condition is true, then print the following
+      printf("a is less than 20\n" );
+   }
+   return 0;
 }
---
+++ 12_If_Else_Statements.c/Example3.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main() {
+       int a, b, c;
+       
+       a = 10; b = 5; c = 7;
+
+       if (a>=b && a>= c){
+        printf("a is greater than b and c \n");
+      
+    }
+    printf("a: %d b: %d c:%d", a, b, c);
+
+    return 0;
+}
