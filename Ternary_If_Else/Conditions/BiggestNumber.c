58c4fd647ebdade9943caf686b199115781ceba0
Manas Tanmay <manas260364@gmail.com>
2026-08-11 19:52:24 +0530

    Reupload

---
+++ Ternary_If_Else/Conditions/BiggestNumber.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main() {
+    int a = 50;
+    int b = 30;
+
+    // Store the bigger number in c
+    int c = (a > b) ? a : b;
+
+    printf("Bigger number = %d", c);
+
+    return 0;
+}
+
+// Reupload
