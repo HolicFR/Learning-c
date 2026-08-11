16620b8855d90236413bbdf9ee4ca9febd437392
Manas Tanmay <manas260364@gmail.com>
2026-08-11 19:52:33 +0530

    Reupload

---
+++ Ternary_If_Else/Conditions/Even_Odd.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main() {
+    int num = 12;
+
+    // If remainder is 0 → Even
+    // Otherwise → Odd
+    (num % 2 == 0) ? printf("Even") : printf("Odd");
+
+    return 0;
+}
+
+// Reupload
