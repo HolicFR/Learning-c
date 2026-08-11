4cbfc8ff2ac9e6484fefe87cf7462e9cbcca52e7
Manas Tanmay <manas260364@gmail.com>
2026-08-11 19:52:09 +0530

    Reupload

---
+++ Ternary_If_Else/Conditions/Adult_minor.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main() {
+    int age = 16;
+
+    // If age >= 18 → Adult
+    // Otherwise → Minor
+    (age >= 18) ? printf("Adult") : printf("Minor");
+
+    return 0;
+}
+
+// Reupload
