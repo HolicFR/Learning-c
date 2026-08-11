42860a842bc45aabf477e23162405d93f1fc9e24
Manas Tanmay <manas260364@gmail.com>
2026-08-11 19:52:46 +0530

    Reupload

---
+++ Ternary_If_Else/Conditions/Pass_fail.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main() {
+    int marks = 35;
+
+    // 40 or above → Pass
+    // Below 40 → Fail
+    (marks >= 40) ? printf("Pass") : printf("Fail");
+
+    return 0;
+}
+
+// // Reupload
