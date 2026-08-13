82bbf6ee818ab17b9723ef414ee71807dc47c43d
Manas Tanmay <manas260364@gmail.com>
2026-08-13 23:35:51 +0530

    Weekly Test 2

---
+++ WeeklyTest_2/Test1.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main() {
+    int age = 19;
+    float height = 5.7;
+    double percentage = 85.5;
+
+    printf("Age = %d\n", age);
+    printf("Height = %.1f\n", height);
+    printf("Percentage = %.2f\n", percentage);
+    
+    return 0;
+}
