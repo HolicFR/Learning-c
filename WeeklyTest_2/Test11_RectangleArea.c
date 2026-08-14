2f16e9384939cf3c1d2344d4b5c7bb396d9d1ba3
Manas Tanmay <manas260364@gmail.com>
2026-08-14 23:42:17 +0530

    feat: Rectangle area calculator - basic arithmetic operations

---
+++ WeeklyTest_2/Test11_RectangleArea.c
@@ -0,0 +1,17 @@
+/*
+ * Program: Calculate Area of Rectangle
+ * Concept: Basic arithmetic operations and variable usage
+ * Author: C Basics Practice
+ */
+
+#include <stdio.h>
+
+int main() {
+    // Hard-coded rectangle dimensions
+    int length = 10;
+    int breadth = 20;
+
+    printf("The area of Rectangle is: %d\n", length * breadth);
+
+    return 0;
+}
