76d1d999888ded86a295a93924d1eb1680206a24
Manas Tanmay <manas260364@gmail.com>
2026-08-14 23:42:44 +0530

    feat: Boolean comparison operations - comparison operators

---
+++ WeeklyTest_2/Test16_BooleanComparison.c
@@ -0,0 +1,19 @@
+/*
+ * Program: Boolean Comparison Operations
+ * Concept: Boolean data type, comparison operators
+ * Author: C Basics Practice
+ */
+
+#include <stdio.h>
+#include <stdbool.h>
+
+int main() {
+    int a = 50;
+    int b = 30;
+
+    bool result = a > b;
+
+    printf("a is greater than b: %d\n", result);
+
+    return 0;
+}
