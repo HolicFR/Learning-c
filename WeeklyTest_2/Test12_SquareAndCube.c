e34b58695315e61a66233fb62aae3f2574ff8f3d
Manas Tanmay <manas260364@gmail.com>
2026-08-14 23:42:20 +0530

    feat: Square and cube calculator - power operations

---
+++ WeeklyTest_2/Test12_SquareAndCube.c
@@ -0,0 +1,17 @@
+/*
+ * Program: Calculate Square and Cube of a Number
+ * Concept: Arithmetic operations, formatting output
+ * Author: C Basics Practice
+ */
+
+#include <stdio.h>
+
+int main() {
+    int n = 7;
+
+    printf("Number: %d\n", n);
+    printf("Square: %d\n", n * n);
+    printf("Cube: %d\n", n * n * n);
+
+    return 0;
+}
