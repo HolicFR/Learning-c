774fb9f10a851969f960c51865162a91d683c8ec
Manas Tanmay <manas260364@gmail.com>
2026-08-14 23:43:06 +0530

    feat: Cylinder volume calculator - geometric formulas with input

---
+++ WeeklyTest_2/Test19_CylinderVolume.c
@@ -0,0 +1,22 @@
+/*
+ * Program: Calculate Volume of Cylinder
+ * Concept: User input, formula implementation, floating-point output
+ * Author: C Basics Practice
+ */
+
+#include <stdio.h>
+
+int main() {
+    int r;
+    int h;
+
+    printf("Input Radius of the circle: ");
+    scanf("%d", &r);
+
+    printf("Input Height: ");
+    scanf("%d", &h);
+
+    printf("The volume of cylinder is: %.1f\n", 3.14 * r * r * h);
+
+    return 0;
+}
