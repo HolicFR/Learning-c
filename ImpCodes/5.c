44877688fe0c135a2bc8e9a786c8942138cc3adf
HolicFR <HolicFR@users.noreply.github.com>
2026-08-15 13:05:00 +0530

    Add GCD calculator example

---
+++ ImpCodes/5.c
@@ -0,0 +1,3 @@
+﻿#include <stdio.h>
+int gcd(int a, int b){ while(b){ int t = a % b; a = b; b = t; } return a; }
+int main(void){ int a, b; printf("Enter two numbers: "); scanf("%d %d", &a, &b); printf("GCD = %d\n", gcd(a, b)); return 0; }
