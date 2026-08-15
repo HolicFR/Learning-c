fa5391f769fd72c4dce4fcc4237938bc5d1ca290
HolicFR <HolicFR@users.noreply.github.com>
2026-08-15 13:09:00 +0530

    Add simple interest calculator example

---
+++ ImpCodes/9.c
@@ -0,0 +1,2 @@
+﻿#include <stdio.h>
+int main(void){ float p, r, t, si; printf("Enter principal rate time: "); scanf("%f %f %f", &p, &r, &t); si = (p * r * t) / 100; printf("Simple Interest = %.2f\n", si); return 0; }
