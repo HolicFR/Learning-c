3f7bce4a451480521b183dfa774ed81b3d0db67e
HolicFR <HolicFR@users.noreply.github.com>
2026-08-15 13:06:00 +0530

    Add LCM calculator example

---
+++ ImpCodes/6.c
@@ -0,0 +1,3 @@
+﻿#include <stdio.h>
+int gcd(int a, int b){ while(b){ int t = a % b; a = b; b = t; } return a; }
+int main(void){ int a, b; printf("Enter two numbers: "); scanf("%d %d", &a, &b); printf("LCM = %d\n", (a / gcd(a, b)) * b); return 0; }
