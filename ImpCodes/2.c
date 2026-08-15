fe30e71104eb51153473894f0d76cb20b03c5fc2
HolicFR <HolicFR@users.noreply.github.com>
2026-08-15 23:14:49 +0530

    1

--- ImpCodes/2.c
+++ ImpCodes/2.c
@@ -12,3 +12,4 @@ int main() {
 //This throws an error because variable is likely to change.
 
 // You should always declare a variable as const when you have values that are unlikely to change:
+// 1
