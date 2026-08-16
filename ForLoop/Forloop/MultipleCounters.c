1dda27a5455bd618c1daa4bd28e11b2285c98d7d
HolicFR <HolicFR@users.noreply.github.com>
2026-08-16 23:45:16 +0530

    Loops with Multiple Counters

---
+++ ForLoop/Forloop/MultipleCounters.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main() {
+    int a, b;
+
+    // for loop execution
+    for(a = 1, b = 1; a <= 5; a++, b++){
+        printf("a: %d b: %d a*b: %d\n", a, b, a*b);
+        
+    }
+    return 0;
+}
