2d23ebf8906e7de2cfed770098564fdf48919949
HolicFR <HolicFR@users.noreply.github.com>
2026-08-16 23:40:35 +0530

    updating loop Counter inside for loop body

---
+++ ForLoop/Forloop/InsideLoopBody.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main() {
+
+        int a;
+
+        // for loop execution
+        for(a = 1; a <= 5; ){
+            printf("a: %d\n", a);
+            a++;
+        }
+    return 0;
+}
