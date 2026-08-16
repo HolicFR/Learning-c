6d735850aa9f9f0cdad31200f7bd0c46398e2b82
HolicFR <HolicFR@users.noreply.github.com>
2026-08-16 23:56:23 +0530

    Syntax of c while loop

--- ForLoop/Forloop/ExampleFactorial.c
+++ ForLoop/Forloop/ExampleFactorial.c
@@ -1,15 +1,15 @@
 #include <stdio.h>
 
-int main() {
-    
-    int i, x = 5;
-    int fact = 1;
+int main(){
 
-    // for loop execution
-    for(i=1; i<=x; i++){
-        fact *= 1;
-    }
-    printf("%d != %d", x, fact);
-    
-    return 0;
+   int i, x = 5;
+   int fact = 1;
+
+   // for loop execution
+   for(i=1; i<=x; i++){
+      fact *= i;
+   }
+   printf("%d != %d", x, fact);
+
+   return 0;
 }
